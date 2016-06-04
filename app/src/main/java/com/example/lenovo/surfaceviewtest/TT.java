f7c79f173051a517fe3165877e045b4afa72e920
aa <1044393721@qq.com>
2016-06-04 18:58:27 +0800

    Testqw

--- app/src/main/java/com/example/lenovo/surfaceviewtest/TT.java
+++ app/src/main/java/com/example/lenovo/surfaceviewtest/TT.java
@@ -4,4 +4,8 @@ package com.example.lenovo.surfaceviewtest;
  * Created by lenovo on 4/6/2016.
  */
 public class TT {
+    public void sayhello()
+    {
+        System.out.println("保存在本地试试看");
+    }
 }
